2f0e9b0843ec9bb112a7a38a670ef784ad972d38
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-11-09 18:46:51 +0530

    changes15

--- database/migrations/2024_11_09_131535_add_meta_tag_to_posts_table.php
+++ database/migrations/2024_11_09_131535_add_meta_tag_to_posts_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('yoga_poses', function (Blueprint $table) {
-            //
+
             $table->dropColumn('meta_tag');
         });
     }
